413206fd3e433e9f62f33b35a0968f302c6dc225
Matthew Gray <matthewcharlesgray319@gmail.com>
2022-09-13 11:58:23 -0400

    Updated.

---
+++ Fall 2022 Classes/Fall 2022 Classes/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall_2022_Classes
+{
+    internal class Car
+    {
+        // properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Speed { get; set; }
+        public bool IsPayedFor { get; set; }
+
+        //access modifiers: public vs. private vs. internal
+        // How we access classes, properities, methods, etc. in our application
+        
+        // constructor
+        public  Car()
+        {
+            Make = "Lexus";
+            Model = "Rx 350";
+            Color = "White";
+            Year = 2018;
+            Speed = 0;
+            IsPayedFor = false;
+        }
+
+        public Car(string make, string model, string color, int year, int speed, bool isPayedFor)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+            Speed = speed;
+            IsPayedFor = isPayedFor;
+
+
+        } 
+
+        // methods
+    }
+}
---
+++ Fall 2022 Classes/Fall 2022 Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Fall_2022_Classes;
+
+// creating a car with our default constructor
+Car ashleysCar = new Car();
+Console.WriteLine("Ashley has a " + ashleysCar.Make);
+
+Console.WriteLine(ashleysCar.Make);
+
+ashleysCar.IsPayedFor = true;
+Console.WriteLine("This car is paid for? " + ashleysCar.IsPayedFor);
+
+// creating another car with a different constructor
+Car gavinsCar = new Car("Toyota","Camry","Grey", 2007, 0, true);
+
+Console.WriteLine("Gavin has a " + gavinsCar.Make);
+
+// making a list of objects from new class
+var garage = new List<Car>();
+
+// adding our cars to the list
+garage.Add(ashleysCar);
+garage.Add(gavinsCar);
+
+// showing we can access those cars and our properties from the list
+Console.WriteLine(garage[1].Model);
